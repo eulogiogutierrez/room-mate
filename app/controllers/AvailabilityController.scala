e707ba0dd30dc18f85b29c3ee12d51328e03bd63
Eulogio Gutierrez <eulogio.gutierrez@digital.hmrc.gov.uk>
2018-02-27 13:39:04 +0000

    basic structure for availability engine

--- app/controllers/AvailabilityController.scala
+++ app/controllers/AvailabilityController.scala
@@ -2,10 +2,14 @@ package controllers
 
 import javax.inject._
 
-import models.{Room, RoomAvailabilityRequest}
+import com.fasterxml.jackson.databind.JsonNode
+import models.{Booking, Room, RoomAvailabilityRequest}
+import org.joda.time.DateTime
+import play.api.libs.json.Json
 import play.api.mvc._
 
 import scala.concurrent.Future
+import scala.io.Source
 
 @Singleton
 class AvailabilityController @Inject() extends Controller {
@@ -21,7 +25,42 @@ class AvailabilityController @Inject() extends Controller {
   }
 
   private def listOfAvailableRooms(roomAvailabilityRequest: RoomAvailabilityRequest): List[Room] = {
+
+    val allBookings: List[Booking] = readBookings()
+    val allRooms: List[Room] = readRooms()
+
+    val startDate = roomAvailabilityRequest.startDate
+    val endDate = roomAvailabilityRequest.startDate.plusMinutes(roomAvailabilityRequest.duration)
+
+    // look at free rooms by startdate and duration
+    /*allBookings
+      .filter(b => startDate.i)
+      .filter(b => )*/
+
+    // look at number of guests
+
+    // look at equipment
+
+    // look at roomType
+
     List()
+  }
+
+
+  private def readBookings(): List[Booking] = {
+    val jValue = Json.parse(Source.fromFile(getClass.getResource("/conf/bookings.json").getPath).mkString)
+    jValue.validate[List[Booking]].fold(
+      errors => throw new Exception("Cannot read bookings."),
+      bookings => bookings
+    )
+  }
+
+  private def readRooms(): List[Room] = {
+    val jValue = Json.parse(Source.fromFile(getClass.getResource("/conf/catalogue.json").getPath).mkString)
+    jValue.validate[List[Room]].fold(
+      errors => throw new Exception("Cannot read catalogue."),
+      rooms => rooms
+    )
   }
 
 }
--- app/models/RoomAvailabilityRequest.scala
+++ app/models/RoomAvailabilityRequest.scala
@@ -2,9 +2,8 @@ package models
 
 import org.joda.time.DateTime
 
-case class RoomAvailabilityRequest (startDate: DateTime,
-                                    duration: Int = 60,
-                                    roomType: String = "any",
-                                    maxNumberOfGuests: Int = 10,
-                                    owner: String,
-                                    equipment: Option[Equipment] = None)
+case class RoomAvailabilityRequest(startDate: DateTime,
+                                   duration: Int = 60,
+                                   roomType: String = "any",
+                                   maxNumberOfGuests: Int = 10,
+                                   equipment: Option[Equipment] = None)
